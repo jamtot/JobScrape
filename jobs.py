18988e0a5eac5795ac16ed4353f0d649b956ee5f
Jonathan Morris <emailforjmorris@gmail.com>
2019-12-12 00:22:58 +1000

    Added a try/except for an issue I can't always replicate. Sometimes the company list falls short due to companies showing as a link throwing off the scraping code. Once I added the new code, the issue stopped, so I don't think it works, but can't test unless it happens again. Silly, huh?

--- jobs.py
+++ jobs.py
@@ -4,13 +4,12 @@ from bs4 import BeautifulSoup
 def scrapeIndeed():
     #collect what jobs the user is looking for and where
 
-    what = input("What job are you looking for? ")
-    #what = "software developer" # default for developing
-    where = input("Where are you looking for the job? ")
-    #where = "brisbane" # default for developing
+    #what = input("What job are you looking for? ")
+    what = "software developer" # default for developing
+    #where = input("Where are you looking for the job? ")
+    where = "brisbane" # default for developing
 
     perpage=100
-    
 
     page = requests.get("https://au.indeed.com/jobs?q="+what+"&l="+where+"&limit="+str(perpage))
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -24,14 +23,20 @@ def scrapeIndeed():
     #companies posts are listed under
     companys = soup.findAll(True, {'class':['company']})
 
+    comp=""
     for i in range(len(listings)):
         lblock = listings[i].find_all("div", {"class": "title"})
         title=(lblock[0].find('a').text).strip()
         place=(places[i].text).strip()
-        comp=(companys[i].text).strip()
+        try:
+            comp=(companys[i].text).strip()
+        except:
+            comp = soup.findAll('class')['company']
         print(str(i+1)+": "+title+"\n"+place+"\n"+comp+"\n")
 
+
     # TODO 
+    # figure out issue with company name not working if it's a link
     # write into CSV file
     # take more information
     # split out skills sought      
