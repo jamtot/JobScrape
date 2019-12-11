fd75fcea38d05500ddcbea116579e3b0d2dfda35
Jonathan Morris <emailforjmorris@gmail.com>
2019-12-11 21:08:08 +1000

    Initial commit for a basic job-site scraper (currently using indeed.com.au)

---
+++ jobs.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrapeIndeed():
+    #collect what jobs the user is looking for and where
+
+    what = input("What job are you looking for? ")
+    #what = "software developer" # default for developing
+    where = input("Where are you looking for the job? ")
+    #where = "brisbane" # default for developing
+
+    perpage=100
+    
+
+    page = requests.get("https://au.indeed.com/jobs?q="+what+"&l="+where+"&limit="+str(perpage))
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    #finds the job listings on the page
+    listings = soup.findAll(True, {'class':['jobsearch-SerpJobCard', 'unifiedRow', 'row', 'result', 'clickcard']})
+
+    #places jobs are listed as
+    places = soup.findAll(True, {'class':['location', 'accessible-contrast-color-location']})
+
+    #companies posts are listed under
+    companys = soup.findAll(True, {'class':['company']})
+
+    for i in range(len(listings)):
+        lblock = listings[i].find_all("div", {"class": "title"})
+        title=(lblock[0].find('a').text).strip()
+        place=(places[i].text).strip()
+        comp=(companys[i].text).strip()
+        print(str(i+1)+": "+title+"\n"+place+"\n"+comp+"\n")
+
+    # TODO 
+    # write into CSV file
+    # take more information
+    # split out skills sought      
+
+if __name__=="__main__":
+    scrapeIndeed()
